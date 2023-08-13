63a339bb0b3a8943e90cbc7c251ddae280309a07
梦念逍遥 <103258286+MengNianxiaoyao@users.noreply.github.com>
2023-08-13 11:53:48 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -14,7 +14,7 @@ let type = "playlist"; //song: 单曲; playlist: 歌单; album: 唱片
 let id = "8598424711"; //封面 ID / 单曲 ID / 歌单 ID
 
 $.ajax({
-    url: "https://api.wuenci.com/meting/api/?server=" + server + "&type=" + type + "&id=" + id,
+    url: "http://apimeting.mnxy.eu.org/api/?server=" + server + "&type=" + type + "&id=" + id,
     type: "GET",
     dataType: "JSON",
     success: function (data) {
